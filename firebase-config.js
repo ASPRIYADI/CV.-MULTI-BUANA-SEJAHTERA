4551b643a7c40483d70c49aba75a5960aa04de33
ASPRIYADI <111231032@mahasiswa.undira.ac.id>
2026-09-21 00:31:46 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCyPdc5rOrft1rMn2qdEQ0FcvqIqgbJDlQ",
+  authDomain: "cv-multi-buana-sejahtera.firebaseapp.com",
+  projectId: "cv-multi-buana-sejahtera",
+  storageBucket: "cv-multi-buana-sejahtera.firebasestorage.app",
+  messagingSenderId: "484453433521",
+  appId: "1:484453433521:web:bf03430237af77b0ab2290",
+  measurementId: "G-HFNBEQPVC8"
+};
